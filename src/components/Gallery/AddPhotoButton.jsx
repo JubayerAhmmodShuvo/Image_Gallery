3eea958c398509bcc08c76a08f1f718e23c06abf
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-11-04 00:13:01 +0600

    add button fixed

--- src/components/Gallery/AddPhotoButton.jsx
+++ src/components/Gallery/AddPhotoButton.jsx
@@ -2,10 +2,24 @@ import React from "react";
 
 const AddPhotoButton = ({ onClick, imageInputRef, handlePhotoUpload }) => {
   return (
-    <>
+    <div
+      className="grid-cell"
+      style={{
+        position: "relative",
+        width: "100%",
+        paddingBottom: "100%", 
+      }}
+    >
       <div
-        className="add-photo-box cursor-pointer flex justify-center items-center text-lg font-serif text-red-500 align-middle rounded box-border h-48 w-48 p-8 border-2 border-gray-300 "
+        className="add-photo-box cursor-pointer flex justify-center items-center text-lg font-serif text-red-500 align-middle rounded box-border p-8 border-2 border-gray-300 "
         onClick={onClick}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
       >
         Add Photo
       </div>
@@ -17,7 +31,7 @@ const AddPhotoButton = ({ onClick, imageInputRef, handlePhotoUpload }) => {
         onChange={handlePhotoUpload}
         style={{ display: "none" }}
       />
-    </>
+    </div>
   );
 };
 
